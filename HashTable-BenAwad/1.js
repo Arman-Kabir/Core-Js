e7e2343c7dd2f78d4fc1a07257a19ee01ea255a0
Arman-Kabir <armankabir8@gmail.com>
2022-12-29 20:04:55 +0600

    obj intro

---
+++ HashTable-BenAwad/1.js
@@ -0,0 +1,10 @@
+// u may know it as a object, may be a map,dictionary or
+//  sometimes a HashTable.They all are basically the same thing
+
+// const person = {}
+// person['firstName'] = 'bob';
+// person['lastName'] = 'bobby';
+
+// console.log(person['firstName']);
+
+
