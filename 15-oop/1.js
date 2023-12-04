d2926992e0cf5be0c54ee814b0568e34ba4b3603
Arman-Kabir <armankabir8@gmail.com>
2023-12-04 10:18:27 +0600

    intro

---
+++ 15-oop/1.js
@@ -0,0 +1,18 @@
+/*1
+    Object - It can have properties like a lens and it can have methods to things it can do.
+
+    Array in js is a type of object in js
+    Any methods that a particular object has access to are listed inside it's ---> __proto__ property
+
+    When we see " new " somewhere -> we know we need to create some kind of object.
+
+    const ages = new Array(10,20,30);  This is array constructor. TO create array
+    
+    literal syntax like curly braces --> {}
+
+    * Many data types in js are not objects --> strings, numbers, booleans, null, undefined all the primitive types don't fall under the object category. But, js wraps the primitive value into a wrapper object and uses properties and methods on the object. null and undefined don't have access to any properties and methods. 
+
+
+
+*/  
+
