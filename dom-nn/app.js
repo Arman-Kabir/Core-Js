749c9b87d0468e735710f14840840f74077f8e95
Arman-Kabir <armankabir8@gmail.com>
2023-12-08 10:08:34 +0600

    1-3

---
+++ dom-nn/app.js
@@ -0,0 +1,25 @@
+// #1
+ // document.getElementById('page-banner')
+// var banner = document.getElementById('page-banner')
+// var bookList = document.getElementById('book-list')
+// console.log(banner);
+
+// #2
+// document.getElementsByClassName('title')
+// var titles = document.getElementsByClassName('title')
+// var lis = document.getElementsByTagName('li')
+// lis[3]
+// for (i = 0; i < titles.length; i++) {
+//     console.log(titles[i])
+// }
+
+// #3
+// var titles = document.getElementsByClassName('title');
+// // console.log(Array.isArray(titles));
+// // console.log(Array.isArray(Array.from(titles)));
+// Array.from(titles).forEach(function(item){
+//     console.log(item);
+// })
+
+
+
