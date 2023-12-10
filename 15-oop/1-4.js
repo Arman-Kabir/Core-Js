d33944833cd07a1e5be66c8f6d4b4df95017bf00
Arman-Kabir <armankabir8@gmail.com>
2023-12-10 08:46:39 +0600

    class methods and methods chaining done

--- 15-oop/1-4.js
+++ 15-oop/1-4.js
@@ -12,7 +12,8 @@
 
     * Many data types in js are not objects --> strings, numbers, booleans, null, undefined all the primitive types don't fall under the object category. But, js wraps the primitive value into a wrapper object and uses properties and methods on the object. null and undefined don't have access to any properties and methods. 
 
-
+    #3
+    A class is like a blueprint for an object( It describes how one should be made). It describes how to create one with all of its base properties and functionality.
 
 */
 
@@ -25,37 +26,45 @@
     // 2 - it binds the value of 'this' to the new empty object.
     // 3 - it calls the constructor function to 'build' the object
 
+    **When we create object using class, we say we are creating an instance of class
     **The instance refers to individual object that we actually create using a class.
  * 
  */
 
 /**#5
-  
+  **When a js method doesn't explicitly return a value, automatically js assigns the return value to be undefined..
+  * this refers ot the object instance
  
  */
 class User {
-    constructor(username,email) {
+    constructor(username, email) {
         // set up properties
         this.username = username;
         this.email = email;
-        this.score  = 0;
+        this.score = 0;
     }
 
-    login(){
+    login() {
         console.log(`${this.username} just logged in`);
+        return this;
     }
-    logout(){
+    logout() {
         console.log(`${this.username} just logged out`);
-    }
-    incScore(){
-        this.score  +=1;
+        return this;
+    } 
+    incScore() {
+        this.score += 1;
         console.log(`${this.username} has a score of ${this.score}`);
+        return this;
     }
 }
 
-const userOne = new User('mario','mario@g.com');
-const userTwo = new User('luigi','luigi@g.com');
+const userOne = new User('mario', 'mario@gmail.com');
+const userTwo = new User('luigi', 'luigi@gmail.com');
 
-console.log(userOne,userTwo);
-userOne.incScore();
-userOne.incScore();
+console.log(userOne, userTwo);
+
+userOne.login().incScore().incScore().logout();
+
+// userOne.incScore().incScore();
+// userOne.incScore();
