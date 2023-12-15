1c5985ed68e088d50a3ba35184bfe6a1d631efa1
Arman-Kabir <armankabir8@gmail.com>
2023-12-15 19:40:25 +0600

    number button added to js variable and shown

--- calc-oop/index.js
+++ calc-oop/index.js
@@ -15,12 +15,15 @@ class Calculator {
 
     }
 
-    appendNumber() {
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
 
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+        console.log(this.currentOperand);
     }
 
-    chooseOperator() {
-
+    chooseOperator(operation) {
+        this.operation = operation;
     }
 
     compute() {
@@ -28,15 +31,42 @@ class Calculator {
     }
 
     updateDisplay() {
-
+        this.currentOperandTextElement.innerText = this.currentOperand;
     }
 }
 
 
 const numberButtons = document.querySelectorAll('[data-number]');
+
+// numberButtons.forEach((button) => {
+//     button.addEventListener('click',()=>{
+//         console.log('clicked');
+//     })
+// })
+
 const operationButtons = document.querySelectorAll('[data-operation]');
+
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+console.log(currentOperandTextElement.textContent);
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // console.log('button clicked');
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // console.log('button clicked');
+        calculator.chooseOperator(button.innerText);
+        calculator.updateDisplay();
+    })
+})
